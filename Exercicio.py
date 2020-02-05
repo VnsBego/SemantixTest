fb72bbbaba1e7262cdf673bd724951e564044d5e
vinicius.bego <vinicius_bego@hotmail.com>
2020-02-05 21:42:20 -0200

    Alterando modelo do primeiro commit, para se ter uma melhor legibilidade do codigo.
    Problema faltante: QUANTIDA DE ​​​​ERROS​ ​404 ​​POR ​​DIA

--- Exercicio.py
+++ Exercicio.py
@@ -1,15 +1,34 @@
-from pyspark import SparkContext
+from pyspark.sql.session import *
+from pyspark.sql import functions as F
 
-sc = SparkContext("local","SemantixTest")
+FILES_PATH = "C://Users//vinicius.bego//Desktop//projetos//teste//SemantixTest//"
 
-#ARQUIVOS NA RAIZ DO PROJETO
-text_jul = sc.textFile("access_log_Jul95")
-text_aug = sc.textFile("access_log_Aug95")
+spark = SparkSession\
+    .builder \
+    .appName("Semantix Test") \
+    .config("spark.master", "local") \
+    .getOrCreate()
 
+df = spark.read.option("delimiter", ' ').csv(FILES_PATH)
+# df.show()
 
 #TOTAL DE HOSTS UNICOS
-text_jul_count = text_jul.flatMap(lambda line: line.split(' ')[0]).distinct().count()
-text_aug_count = text_aug.flatMap(lambda line: line.split(' ')[0]).distinct().count()
+df_unic_host = df.select("_c0").distinct()
+print(df_unic_host.count())
 
-print("Distinct Hosts JUL: %s" %text_jul_count)
-print("Distinct Hosts AUG: %s" %text_aug_count)
+#TOTAL DE ERROS 404
+df_error404 = df.select("_c6").filter("_c6 == 404")
+print(df_error404.count())
+
+#TOP 5 COM MAIS 404
+df_top5_erro404 = df.select("_c0").filter("_c6 == 404")
+df_top5_erro404.groupBy("_c0").count().sort("count", ascending=False).show(5, False)
+
+#QUANTIDADE DE ​​​​ERROS​ ​404 ​​POR ​​DIA
+#df_day_count_erro404 = df.withColumn("novotime",col('_c3').cast('timestamp')).show()
+#df.select("_c3").filter("_c6 == 404").todate("date", "yyyyMMdd")
+#df_day_count_erro404.groupBy("_c3").count().sort("count", ascending=False).show(20, False)
+
+#TOTAL ​​DE​​ BYTES ​​RETORNADOS
+df_sum_bytes = df.agg(F.sum("_c7")).collect()
+print(df_sum_bytes)
